414bb1d5f5534831448fe7443dd2b24d942c9a29
Leonard Jones <lendjones@Leonards-Mac-mini.local>
2018-02-28 23:16:18 -0500

    add base and index templates to gardens

--- hansberry/gardens/views.py
+++ hansberry/gardens/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the gardens index")
+    return render(request, 'gardens/index.html')
 
 
