b6b82d5399ad729968e9a778dea788c9979b15c7
Leonard Jones <lendjones@Leonards-Mac-mini.local>
2018-03-03 14:23:57 -0500

    add test for displaying logged in user's gardens

--- hansberry/gardens/tests/test_views.py
+++ hansberry/gardens/tests/test_views.py
@@ -58,7 +58,12 @@ class GardenOwnerListViewTest(TestCase):
             name='Hansberry Garden and Nature Center',
             address=self.gardenaddress_hansberry)
 
-    def test_login(self):
+        # create more test gardens
+        self.test_garden = Garden.objects.create(name='Test Garden')
+        self.test_garden_2 = Garden.objects.create(name='Test Garden 2')
+        self.test_garden_3 = Garden.objects.create(name='Test Garden 3')
+
+    def test_view_url_accessible_by_name(self):
         """Tests that garden user can login"""
         login = self.client.login(username='testuser', password='secret')
         url = reverse('my-gardens')
@@ -66,3 +71,47 @@ class GardenOwnerListViewTest(TestCase):
         self.assertEqual(resp.status_code, 200)
         # check if user is logged in
         self.assertEqual(str(resp.context['user']), 'testuser')
+
+    def test_redirect_if_not_logged_in(self):
+        """Test that user is redirected to login page"""
+        test_garden = self.hansberry_garden
+        url = reverse('my-gardens')
+        resp = self.client.get(url)
+        self.assertRedirects(resp, '/accounts/login/?next=/mygardens/')
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(username='testuser', password='secret')
+        test_garden = self.hansberry_garden
+        url = reverse('my-gardens')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        # check if user is logged in
+        self.assertEqual(str(resp.context['user']), 'testuser')
+        # check its using the correct template
+        self.assertTemplateUsed(resp, 'gardens/gardens_list_user.html')
+
+    def test_lists_user_garden(self):
+        # test all user's gardens are listed
+        login = self.client.login(username='testuser', password='secret')
+        test_garden = self.hansberry_garden
+        # associate user with garden
+        test_garden.garden_author = User.objects.get(username='testuser')
+        test_garden.save()
+        url = reverse('my-gardens')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.context['object_list']), 1)
+
+    def test_lists_multiple_user_gardens(self):
+        # test all user's gardens are listed
+        login = self.client.login(username='testuser', password='secret')
+        # associate user with multiple gardens
+        test_gardens = (self.test_garden, self.test_garden_2,
+                        self.test_garden_3)
+        for garden in test_gardens:
+            garden.garden_author = User.objects.get(username='testuser')
+            garden.save()
+        url = reverse('my-gardens')
+        resp = self.client.get(url)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.context['object_list']), 3)
